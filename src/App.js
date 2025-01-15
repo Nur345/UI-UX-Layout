9bba69ff0e4254e21c4d3a13e87882661d61be82
Noor Saba <159886547+Nur345@users.noreply.github.com>
2025-01-15 13:21:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react'
-// import FlexBox from './FlexBox/FlexBox'
 import CommunitiesTrending from './CommunitiesTrending/CommunitiesTrending'
-
 const App = () => {
   return (
     <div>
-      {/* <FlexBox /> */}
       <CommunitiesTrending />
     </div>
   )
